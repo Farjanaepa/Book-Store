4ff13bb7a4656487243218caaeef771b2ef4e9cd
Farjanaepa <farjanaepa@gmail.com>
2025-10-05 23:55:39 +0600

    Header footer set start

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+          <h1>Hellow i am home</h1>  
+        </div>
+    );
+};
+
+export default Home;<h1>Hellow i am home</h1>
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { createBrowserRouter } from "react-router";
+import Root from '../Pages/Root/Root';
+import Errorpage from '../Pages/ErrorPage/Errorpage';
+import Home from '../Pages/Home/Home';
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root ,
+    errorElement: <Errorpage></Errorpage>,
+    children: [
+        {
+            index: true,
+            path:"/",
+            Component: Home
+        }
+    ]
+  },
+]);
