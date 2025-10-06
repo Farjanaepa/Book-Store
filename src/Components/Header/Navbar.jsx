d8cb0a8aaa21f6c602112044646e845a6a724fb0
Farjana Epa <farjanaepa@gmail.com>
2025-10-06 15:27:39 +0600

    li set

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Navbar = () => {
+
+  const links = <>
+  <li>Home</li>
+  <li>About</li>
+  </>
     return (
         
            <div className="navbar bg-base-100 shadow-sm">
@@ -12,15 +17,7 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {links}
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
@@ -28,16 +25,8 @@ const Navbar = () => {
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
       <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      
+      <li><a>Item 2</a></li>
     </ul>
   </div>
   <div className="navbar-end">
